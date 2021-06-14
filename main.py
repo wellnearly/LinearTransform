c80f4c80a2a42c15e293b3252a2c5e52c67a65e1
wellnearly <russu94@gmail.com>
2021-06-14 20:34:50 +0300

    init

---
+++ main.py
@@ -0,0 +1,70 @@
+import cv2
+import numpy as np
+
+
+def add_zeros_plate(img):
+    # Add to the expanded another plane with zeros
+    rows, cols = img.shape
+    print("Высота:" + str(rows))
+    print("Ширина:" + str(cols))
+    m = cv2.getOptimalDFTSize(rows)
+    n = cv2.getOptimalDFTSize(cols)
+
+    padded = cv2.copyMakeBorder(img, 0, m - rows, 0, n - cols, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+
+    planes = [np.float32(padded), np.zeros(padded.shape, np.float32)]
+    return planes
+
+
+def swap_quadrants(mag_img):
+    mag_img_rows, mag_img_cols = mag_img.shape
+    # crop the spectrum, if it has an odd number of rows or columns
+    mag_img = mag_img[0:(mag_img_rows & -2), 0:(mag_img_cols & -2)]
+    cx = int(mag_img_rows / 2)
+    cy = int(mag_img_cols / 2)
+    q0 = mag_img[0:cx, 0:cy]  # Top-Left - Create a ROI per quadrant
+    q1 = mag_img[cx:cx + cx, 0:cy]  # Top-Right
+    q2 = mag_img[0:cx, cy:cy + cy]  # Bottom-Left
+    q3 = mag_img[cx:cx + cx, cy:cy + cy]  # Bottom-Right
+    tmp = np.copy(q0)  # swap quadrants (Top-Left with Bottom-Right)
+    mag_img[0:cx, 0:cy] = q3
+    mag_img[cx:cx + cx, cy:cy + cy] = tmp
+    tmp = np.copy(q1)  # swap quadrant (Top-Right with Bottom-Left)
+    mag_img[cx:cx + cx, 0:cy] = q2
+    mag_img[0:cx, cy:cy + cy] = tmp
+
+
+def log_scale(mag_img):
+    # switch to logarithmic scale
+    mat_of_ones = np.ones(mag_img.shape, dtype=mag_img.dtype)
+    cv2.add(mat_of_ones, mag_img, mag_img)
+    cv2.log(mag_img, mag_img)
+
+
+imgArr = []
+imgArr.append(cv2.imread('img/noise.png', cv2.IMREAD_GRAYSCALE))
+imgArr.append(cv2.imread('img/512x512-Gaussian-Noise.jpg', cv2.IMREAD_GRAYSCALE))
+for img in imgArr:
+    cv2.imshow("Input Image", img)
+
+    planes = add_zeros_plate(img)
+    complexI = cv2.merge(planes)
+
+    cv2.dft(complexI, complexI)  # this way the result may fit in the source matrix
+
+    cv2.split(complexI, planes)  # planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
+    cv2.magnitude(planes[0], planes[1], planes[0])  # planes[0] = magnitude
+    magI = planes[0]
+
+    swap_quadrants(magI)
+    log_scale(magI)
+
+    cv2.normalize(magI, magI, 0, 1, cv2.NORM_MINMAX)  # Transform the matrix with float values into a
+
+    # Show the result
+    cv2.imshow("dft", magI)
+    cv2.waitKey()
+
+
+
+
